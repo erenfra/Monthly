8c2c41cc99331c68b183fac32e56b71a16f0c351
Renato Oliveira Fraga <rofraga74@gmail.com>
2024-06-04 18:01:25 -0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Renato Oliveira Fraga on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
